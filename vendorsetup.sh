0d0eb8f11aaee3bb5e259e536632a9e249c016c5
Patriot <77717199+Patriot-06@users.noreply.github.com>
2022-03-27 16:50:33 +0530

    x3: Clone Kernel Source with --depth=1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,5 @@ sudo rm -r vendor/realme
 sudo rm -r kernel/realme
 git clone https://github.com/Patriot-06/hardware_oplus.git hardware/oplus
 git clone https://github.com/Patriot-06/android_vendor_realme_x3.git vendor/realme/x3
-git clone https://github.com/Patriot-06/android_kernel_realme_x3.git kernel/realme/x3
+git clone --depth=1 https://github.com/Patriot-06/android_kernel_realme_x3.git kernel/realme/x3
 git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git prebuilts/clang/host/linux-x86/clang-15.0
